01143a67366f9198c35c589c670f4cbf628027bb
Juan Molero <juanmolerog@gmail.com>
2016-07-01 00:40:48 -0400

    Added confirmation key generation and some other changes to steam inventory handler

--- SteamInventory.py
+++ SteamInventory.py
@@ -6,8 +6,15 @@ class Inventory(object):
         self.Login = Login
         self.request = Login.request
         self.items = []
-    def getInventory(self,steamID,appID,contextID):
-        response = self.request.get("https://steamcommunity.com/id/" + steamID + "/inventory/json/" + str(appID) + "/" + str(contextID))
+    def getInventory(self,steamID,appID,contextID,tradeable=False):
+        self.steamID = steamID
+        self.appID = appID
+        self.contextID = contextID
+        self.items = []
+        params = {}
+        if tradeable:
+            params['trading'] = 1
+        response = self.request.get("https://steamcommunity.com/id/" + steamID + "/inventory/json/" + str(appID) + "/" + str(contextID),params=params)
         if response.status_code != 200:
             raise Exception("Server Error")
         data = response.json()
@@ -27,15 +34,18 @@ class Inventory(object):
             amount = rgInventory[x]['amount']
             key = str(class_id) + "_" + str(instance_id)
             description = rgDescriptions[key]
-            self.items.append(Item(item_id,class_id,instance_id,amount,description))
+            self.items.append(Item(item_id,contextID,class_id,instance_id,amount,description))
+    def __str__(self):
+        return("Steam Inventory for " + self.steamID + " on App: " + self.appID + " and Context: " + self.contextID)
 
 class Item(object):
-    def __init__(self,item_id,class_id,instance_id,amount,description):
+    def __init__(self,item_id,contextID,class_id,instance_id,amount,description):
         self.itemid = item_id
         self.classid = class_id
         self.instanceid = instance_id
         self.amount = amount
         self.appid = description['appid']
+        self.contextid = contextID
         self.icon_url = "http://cdn.steamcommunity.com/economy/image/" + description['icon_url']
         self.icon_url_large = "http://cdn.steamcommunity.com/economy/image/" + description['icon_url_large'] if 'icon_url_large' in description.keys() else ""
         self.icon_drag_url = description['icon_drag_url']
@@ -53,6 +63,17 @@ class Item(object):
         self.actions = description['actions'] if 'actions' in description.keys() else ""
         self.market_actions = description['market_actions'] if 'market_actions' in description.keys() else ""
         self.tags = description['tags']
+    def getMarketHashName(self):
+        if self.market_hash_name != "":
+            return self.market_hash_name
+        elif self.market_name != "":
+            return self.market_name
+        else:
+            return self.name
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+    def __unicode__(self):
+        return self.getMarketHashName()
 
 class InventoryException(Exception):
     def __init__(self,steamID,appID,contextID):
--- SteamMobileAuth.py
+++ SteamMobileAuth.py
@@ -1,6 +1,7 @@
-import time, hmac, hashlib, requests, pdb
+import time, hmac, hashlib, requests, re, pdb
 from bitstring import BitArray
-from base64 import b64decode
+from base64 import b64decode,b64encode
+from binascii import hexlify
 
 def getTimeOffset():
     url = "http://api.steampowered.com/ITwoFactorService/QueryTime/v1/"
@@ -32,3 +33,31 @@ def generateAuthCode(secret,offset=0):
         code += chars[int(fullcode % len(chars))]
         fullcode = fullcode/len(chars)
     return code
+
+def generateConfirmationKey(identitySecret,time,tag=""):
+    identitysecret = b64decode(identitySecret)
+    secret = BitArray(bytes=identitysecret,length=len(identitysecret)*8)
+    datalen = 8
+    if tag != "":
+        if(len(tag)>32):
+            datalen+=32
+        else:
+            datalen+=len(tag)
+        tagBuff = BitArray(bytes=tag,length=len(tag)*8)
+    buff = BitArray(datalen*8)
+    time = int(time)
+    buff[:-4*8] = time
+    if tag != "":
+        buff[8*8:] = tagBuff
+    conf_hmac = hmac.new(secret.tobytes(),buff.tobytes(),hashlib.sha1)
+    return b64encode(conf_hmac.digest())
+
+def getDeviceID(steamID):
+    hashed_hmac = hmac.new("",str(steamID),hashlib.sha1)
+    hashed_hmac = hexlify(hashed_hmac.digest())
+    match = re.match(r'^([0-9a-f]{8})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{4})([0-9a-f]{12}).*$',hashed_hmac)
+    devid = "android:"
+    for x in match.groups():
+        devid += x
+        devid +="-"
+    return devid[:-1]
